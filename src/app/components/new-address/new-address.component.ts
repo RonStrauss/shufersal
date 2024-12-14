f1febfc85b0df7d51b4242b8925d65dec3f0f8d0
RonStrauss <ashbelthor@gmail.com>
2024-12-14 19:59:08 +0200

    add new address component skeleton

---
+++ src/app/components/new-address/new-address.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-address',
+  imports: [],
+  templateUrl: './new-address.component.html',
+  styleUrl: './new-address.component.scss'
+})
+export class NewAddressComponent {
+
+}
